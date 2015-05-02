dfa4bed2deb74e384584f282a9f90e4954d11d76
CompileMaster <12126769@qq.com>
2015-05-03 02:41:17 +0800

    Update SecurityPolicy-Client.cmd

--- SecurityPolicy-Client.cmd
+++ SecurityPolicy-Client.cmd
@@ -8,6 +8,7 @@ netsh ipsec static add policy name=SecurityPolicy
 netsh ipsec static add filterlist name=PermitList
 netsh ipsec static add filter description="http" filterlist=PermitList srcaddr=me srcport=0 dstaddr=any dstport=80 protocol=tcp mirrored=yes
 netsh ipsec static add filter description="https" filterlist=PermitList srcaddr=me srcport=0 dstaddr=any dstport=443 protocol=tcp mirrored=yes
+netsh ipsec static add filter description="https" filterlist=PermitList srcaddr=me srcport=0 dstaddr=any dstport=443 protocol=udp mirrored=yes
 netsh ipsec static add filteraction name=PermitAction inpass=yes soft=yes action=negotiate
 netsh ipsec static add rule name=PermitRule policy=SecurityPolicy filterlist=PermitList filteraction=PermitAction psk="U2FsdGVkX1+hRdnhQolFUZxHhthrZiZ2H1D6iXIBlg9dTaCUIj4RvVDEsMd9DXDRlE/+QPNIQx6O4IBKGdvA+UPETtCYwdycQy9chWTmdzJRPsD3Ej8A4icIqhNlMZygERC9DgPDCSNX9MoJXFBjqdI8E34Q054ClmHbG2Pt+Q=="
 netsh ipsec static set policy name=SecurityPolicy assign=y
